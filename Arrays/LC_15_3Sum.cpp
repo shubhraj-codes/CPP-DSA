0d6cddda30664d63e12f0fe5dbb97c2896aa5262
Shubh Raj <shubhcsai1@gmail.com>
2026-07-19 13:49:50 +0530

    Create LC_15_3Sum.cpp

---
+++ Arrays/LC_15_3Sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        // -4 -1 -1 0 2 3 
+        int n = nums.size();
+
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1, k = n-1;
+
+            while(j < k)
+            {
+                int value = nums[i] + nums[j] + nums[k];
+                
+                if(value < 0) j++;
+
+                else if(value > 0) k--;
+
+                else
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
